b763d2be2d7bd01b38deb2c19a9c42664be1f94b
Adrian Weber <webrian@openstreetmap.la>
2013-11-06 09:47:27 +0100

    Update the DirectionsController

--- Controller/DirectionsController.php
+++ Controller/DirectionsController.php
@@ -26,61 +26,63 @@ class DirectionsController extends AppController {
         }
 
         // Compose the request URL
-        $requestUrl = "http://openstreetmap.la:4576/viaroute&start=" . $start[0] . "," . $start[1] . "&dest=" . $dest[0] . "," . $dest[1] . "&output=json&instructions=true";
-
-        // Append the via points if requested in the following
-        // format: &via=18,102.6&via=17.9,102.59
+        $requestUrl = "http://localhost:8118/viaroute?loc=" . $start[0] . "," . $start[1];
         if (!empty($viaHashs)) {
             foreach ($viaHashs as $via) {
                 $viaCoordinates = $geohash->decode($via);
-                $requestUrl .= "&via=" . $viaCoordinates[0] . "," . $viaCoordinates[1];
+                $requestUrl .= "&loc=" . $viaCoordinates[0] . "," . $viaCoordinates[1];
             }
         }
+        $requestUrl .= "&loc=" . $dest[0] . "," . $dest[1] . "&output=json&instructions=true";
 
         //echo $requestUrl;
 
         $content = @file_get_contents($requestUrl);
 
-        if (empty($content)) {
-            // Log an error attempt
-            $message = array(
-                'clientIp' => $this->request->clientIp(),
-                'method' => $this->request->method(),
-                'here' => $this->request->here,
-                'referer' => $this->request->referer(),
-                'status' => 401,
-                'filesize' => 0,
-                'text' => "Server is not accessible."
-            );
-            CakeLog::write("apache_error", $message);
-            throw new BadRequestException('Server is not accessible.');
-        }
+        $this->set('data', json_decode($content));
 
-        $resultJson = json_decode($content);
+        /**
+          if (empty($content)) {
+          // Log an error attempt
+          $message = array(
+          'clientIp' => $this->request->clientIp(),
+          'method' => $this->request->method(),
+          'here' => $this->request->here,
+          'referer' => $this->request->referer(),
+          'status' => 401,
+          'filesize' => 0,
+          'text' => "Server is not accessible."
+          );
+          CakeLog::write("apache_error", $message);
+          throw new BadRequestException('Server is not accessible.');
+          }
 
-        $geometry['type'] = "LineString";
+          $resultJson = json_decode($content);
 
-        $coordinates = array();
-        foreach ($resultJson->route_geometry as $coordinate) {
-            array_push($coordinates, array($coordinate[1], $coordinate[0]));
-        }
-        $geometry['coordinates'] = $coordinates;
-        $properties['status'] = $resultJson->status;
-        $properties['status_message'] = $resultJson->status_message;
-        $properties['total_distance'] = $resultJson->route_summary->total_distance;
-        $properties['total_time'] = $resultJson->route_summary->total_time;
-        $properties['transactionId'] = $resultJson->transactionId;
-        $properties['route_instructions'] = $resultJson->route_instructions;
-        //$properties['request_url'] = $requestUrl;
+          $geometry['type'] = "LineString";
 
-        $feature['geometry'] = $geometry;
-        $feature['properties'] = $properties;
-        $feature['type'] = 'Feature';
+          $coordinates = array();
+          foreach ($resultJson->route_geometry as $coordinate) {
+          array_push($coordinates, array($coordinate[1], $coordinate[0]));
+          }
+          $geometry['coordinates'] = $coordinates;
+          $properties['status'] = $resultJson->status;
+          $properties['status_message'] = $resultJson->status_message;
+          $properties['total_distance'] = $resultJson->route_summary->total_distance;
+          $properties['total_time'] = $resultJson->route_summary->total_time;
+          $properties['transactionId'] = $resultJson->transactionId;
+          $properties['route_instructions'] = $resultJson->route_instructions;
+          //$properties['request_url'] = $requestUrl;
 
-        $geoJson['type'] = "FeatureCollection";
-        $geoJson['features'] = array($feature);
+          $feature['geometry'] = $geometry;
+          $feature['properties'] = $properties;
+          $feature['type'] = 'Feature';
 
-        $this->set('data', $geoJson);
+          $geoJson['type'] = "FeatureCollection";
+          $geoJson['features'] = array($feature);
+
+          $this->set('data', $geoJson);
+         */
 
         // Log a successful found route
         $message = array(
@@ -91,7 +93,9 @@ class DirectionsController extends AppController {
             'status' => 200,
             'filesize' => strlen(json_encode($geoJson))
         );
-        CakeLog::write("apache_access", $message);
+        CakeLog::write("apacihe_access", $message);
     }
+
 }
+
 ?>
